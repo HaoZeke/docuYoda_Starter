ee50c6c9f3fe6bcc3bf1ca56bc050e3d430f1733
HaoZeke <driger633@gmail.com>
2017-11-29 06:43:12 +0530

    gulp: Add paths

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,3 +7,32 @@ import gap from 'gulp-append-prepend';
 import insert from 'gulp-insert';
 import { exec } from 'child_process';
 
+const paths = {
+  content: {
+    src: 'src/md',
+    conf: 'src/conf',
+    tex: 'src/tex'
+  },
+  images: {
+    src: 'src/img',
+    dest: 'sap'
+  },
+  styles: {
+    src: 'src/assets/styles/**/*.scss',
+    dest: 'sap/css/'
+  },
+  js: {
+    src: 'src/assets/js',
+    dest: 'sap/js/'
+  },
+  watchfor: {
+    md: 'src/md/**/*.md',
+    conf: 'src/conf/**/*.yml',
+    filters: 'src/filters/**/*.py',
+    refs: 'src/**/*.bib',
+    tex: 'src/tex/**/*.tex',
+    styles: 'src/assets/styles/**/*.scss',
+    js: 'src/assets/js/**/*.js',
+    images: 'src/img/**/*.{jpg,jpeg,png}'
+  }
+}
