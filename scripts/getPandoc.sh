dfa62f0da6b3bf99090e15e0a42e9bb26c61c6de
HaoZeke <rohit.goswami@aol.com>
2017-12-25 16:54:56 +0530

    scripts: Add a full build script

--- scripts/getPandoc.sh
+++ scripts/getPandoc.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 if [ ! -f $HOME/save/pand2.deb ]; then
     aria2c https://github.com/jgm/pandoc/releases/download/2.0.1.1/pandoc-2.0.1.1-1-amd64.deb -d $HOME/save -o pand2.deb
 
