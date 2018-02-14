acf948243d11aa0aee329f43b3ed3872b017c152
HaoZeke <rohit.goswami@aol.com>
2018-02-14 11:36:05 +0530

    Try a deploy script [ci skip]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,14 +9,14 @@ cd sap/pdf
 
 git init
 git config user.name "HaoZeke"
-git config user.email "rohit@myarchbox.com"
+git config user.email "rohit.goswami@aol.com"
 
-git remote add upstream "git@bitbucket.org:HaoZeke/CommaCentral.git"
+git remote add upstream "git@github.com:HaoZeke/docuYoda_Starter.git"
 git fetch upstream
 git reset upstream/pdf
 
 touch .
 
 git add -A .
-git commit -m "Wercker rebuilt the pdf at ${rev}"
+git commit -m "Semaphore rebuilt the pdf at ${rev}"
 git push -q upstream HEAD:pdf
