187c7a39f5e402c69d251e8fe0db6eec183ff2ee
HaoZeke <rohit.goswami@aol.com>
2018-02-14 15:22:25 +0530

    Fix scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,11 +12,11 @@ git config user.name "HaoZeke"
 git config user.email "rohit.goswami@aol.com"
 
 git remote add upstream "git@github.com:HaoZeke/docuYoda_Starter.git"
-git fetch upstream
-git reset upstream/pdf
+git fetch --all
+# git reset upstream/pdf
 
 touch .
 
 git add -A .
 git commit -m "Semaphore rebuilt the pdf at ${rev}"
-git push -q upstream HEAD:pdf
+git push -q upstream HEAD:pdf --force
