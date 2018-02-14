73f89fc59c5fdee592001d88d0a397e792f5014b
HaoZeke <rohit.goswami@aol.com>
2018-02-14 15:08:38 +0530

    Rework logic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,24 +2,21 @@
 
 set -o errexit -o nounset
 
-# rev=$(git rev-parse --short HEAD)
+rev=$(git rev-parse --short HEAD)
 
-# cd sap/pdf
-# rm -rf .git
+cd sap/pdf
+#rm -rf .git
 
-# git init
-# git config user.name "HaoZeke"
-# git config user.email "rohit.goswami@aol.com"
+git init
+git config user.name "HaoZeke"
+git config user.email "rohit.goswami@aol.com"
 
-# git remote add upstream "git@github.com:HaoZeke/docuYoda_Starter.git"
-# git fetch --all
+git remote add upstream "git@github.com:HaoZeke/docuYoda_Starter.git"
+git fetch upstream
+git reset upstream/pdf
 
-# touch .
+touch .
 
-# git add -A .
-# git commit -m "Semaphore rebuilt the pdf at ${rev}"
-# git merge upstream/pdf --allow-unrelated-histories --no-edit
-# git reset upstream/pdf
-# git push -q upstream HEAD:pdf
-
-git subtree push --prefix sap/pdf  origin pdf
+git add -A .
+git commit -m "Semaphore rebuilt the pdf at ${rev}"
+git push -q upstream HEAD:pdf
